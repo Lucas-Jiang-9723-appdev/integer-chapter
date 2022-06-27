83093e7619ff94a6482662d2b93c279af141d2c0
Lucas-Jiang-9723 <yj1084@uchicago.edu>
2022-06-27 19:55:02 +0000

    part4 integer complete

--- integer_birth_year.rb
+++ integer_birth_year.rb
@@ -1,9 +1,12 @@
 # Ask for a person’s age, and then calculate and display what year they were born in.
-# 
+#
 # Input:
 #   80
-# 
+#
 # Key output:
 #   "Wow, you were born in 1940. You're old!"
 
 p "How old are you?"
+age = gets.to_i
+born_year = 2022 - age
+p "Wow, you were born in #{born_year}. You're old!"
